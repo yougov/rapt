0eee6a418257fbd0015489bd10e2bf00a592c0c5
Eric Larson <eric@ionrock.org>
2014-10-08 15:32:16 -0500

    Clean up event filtering

--- rapt/events.py
+++ rapt/events.py
@@ -1,11 +1,4 @@
-import json
 
-# example
-# ex = {"time": "2014-10-08T15:42:33.032207",
-#       "message": "eric.larson swarmed pangaea-3.3.7-prod-worker\n\n        App: pangaea\n        Version: 3.3.7\n        Config Name: prod\n        Proc Name: worker\n        Squad: ldc\n        Memory: \n        Size: 5\n        Balancer: None\n        Pool: None\n",
-#       "tags": ["user", "swarm"],
-#       "title": "eric.larson: swarm pangaea-3.3.7-prod-worker"}
-# {"time": "2014-10-08T15:43:07.689344", "message": "Uptests passed for swarm pangaea-3.3.7-prod-e7977e6f-worker", "tags": ["success", "uptest"], "title": "Uptests passed"}
 
 
 class SwarmEventProps(object):
@@ -27,7 +20,6 @@ class SwarmEventProps(object):
 
 
 class SwarmEvents(SwarmEventProps):
-    format_tmpl = '{time} {title} {tags}'
 
     def __init__(self, swarm, vr):
         self.swarm = swarm
@@ -73,26 +65,35 @@ class SwarmEvents(SwarmEventProps):
 
         return False
 
-    def __iter__(self):
-        """Find the events for a specific swarm."""
+    def __call__(self, event):
         handlers = [
             self.user_swarm_event,
             self.swarm_build_event,
             self.swarm_deploy_event,
         ]
+        for handler in handlers:
+            e = handler(event)
+            if e:
+                return e
 
-        for event in self.vr.events():
-            event = json.loads(event.data)
-            for handler in handlers:
-                e = handler(event)
-                if e:
-                    yield self.format(e)
 
-                if self.done():
-                    break
+FORMAT_TMPL = '{time} {title} {tags}'
 
-    def format(self, event):
-        message = self.format_tmpl.format(**event)
-        if 'failure' in event['tags']:
-            message += '\n\n' + event['message']
-        return message
+
+def format_event(event):
+    message = FORMAT_TMPL.format(**event)
+    if 'failure' in event['tags']:
+        message += '\n\n' + event['message']
+    return message
+
+
+def filtered_events(vr, handlers=None):
+    if not handlers:
+        handlers = [lambda x: x]
+
+    for event in vr.events():
+        print(event)
+        for handler in handlers:
+            message = handler(event)
+            if message:
+                yield format_event(message)
