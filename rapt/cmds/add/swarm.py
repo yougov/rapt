906eda5398767b03c66787052b2c568c4a637bc9
Eric Larson <eric@ionrock.org>
2014-10-09 18:02:11 -0500

    Add swarm creation.

--- rapt/cmds/add/swarm.py
+++ rapt/cmds/add/swarm.py
@@ -1,27 +1,81 @@
 import click
 
 from rapt.connection import get_vr
-from rapt.models import apps
-from rapt.util import edit_yaml, dump_yaml
+from rapt.models import apps, squads, models
+from rapt.util import edit_yaml, dump_yaml, load_yaml
+
+from pprint import pformat
 
 
-@click.command()
-def swarm():
-    """Add a new swarm."""
-    vr = get_vr()
+def validate_uri(name_or_uri, objs):
+    uris = set([s['resource_uri'] for s in objs])
+    names = {s['name']: s['resource_uri'] for s in objs}
 
+    if name_or_uri in names:
+        return names[name_or_uri]
+    assert name_or_uri in uris
+    return name_or_uri
+
+
+def validate(config, squads, apps):
+    config['squad'] = validate_uri(config['squad'], squads)
+    config['app'] = validate_uri(config['app'], apps)
+
+    if 'os_image' not in config:
+        config['os_image'] = None
+    return config
+
+
+def get_config(vr, app_list, squad_list):
     tmpl = {
-        'app_name': '',
+        'app': '',
         'version': '',
         'proc_name': '',
         'size': '',
-        'squad': ''
+        'squad': '',
+        # TODO: Add config options
     }
 
     info = {
-        'available apps': [app['name'] for app in apps(vr)],
+        'NOTES': 'Use the URL when copying vlaues.',
+
+        'available apps': {
+            str(app['name']): str(app['resource_uri'])
+            for app in app_list
+        },
+
+        'available squads': {
+            str(squad['name']): str(squad['resource_uri'])
+            for squad in squad_list
+        },
     }
 
     footer = dump_yaml(info)
-    config = edit_yaml(dump_yaml(tmpl), footer)
-    print(config)
+    return edit_yaml(dump_yaml(tmpl), footer)
+
+
+@click.command()
+@click.argument('config', type=click.File('rb'), nargs=-1)
+def swarm(config):
+    """Add a new swarm."""
+    vr = get_vr()
+
+    app_list = apps(vr)
+    squad_list = squads(vr)
+
+    if config:
+        config = load_yaml(config[0])
+
+    config = config or get_config(vr, app_list, squad_list)
+
+    if config:
+        config = validate(config, squad_list, app_list)
+        click.echo('Creating swarm with following config:\n\n')
+        click.echo(dump_yaml(config))
+        click.echo()
+        click.echo()
+
+        if click.confirm('Add the swarm?', default=True):
+            swarm = models.Swarm(vr, config)
+            swarm.create()
+            click.echo('Swarm %s created!' % (swarm.name))
